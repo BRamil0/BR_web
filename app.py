c80878c060e4b5fce3d62e199ba854e65e7eefe9
Radomyr <qulowg@gmail.com>
2024-10-07 20:11:13 +0300

    11 fix, https://vercel.com/ таке собі

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import uvicorn
 import fastapi
 from fastapi.staticfiles import StaticFiles
-from src.config.config import settings
-
 from src.app.fastapi import main_router
+
 
 def import_routers(app: fastapi.FastAPI) -> None:
     """
@@ -13,24 +11,9 @@ def import_routers(app: fastapi.FastAPI) -> None:
     """
     app.include_router(main_router.router)
 
-def start() -> None:
-    """
-    start of all processes
-    :return: None
-    """
 
-    app = fastapi.FastAPI()
+app = fastapi.FastAPI()
 
-    app.mount("/static", StaticFiles(directory="src/static"), name="static")
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
-    import_routers(app)
-
-    uvicorn.run(app=app,
-                host=settings.HOST,
-                port=settings.PORT,)
-
-if "__main__" == __name__:
-    try:
-        start()
-    except KeyboardInterrupt:
-        pass
+import_routers(app)
