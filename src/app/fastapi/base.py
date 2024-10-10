5bd03b4b42d56ce32f3031b8b53738c5f6db66ca
Radomyr <qulowg@gmail.com>
2024-10-10 18:57:46 +0300

    lang fix

--- src/app/fastapi/base.py
+++ src/app/fastapi/base.py
@@ -1,16 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, Cookie
 from starlette.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
-
 router = APIRouter()
 templates = Jinja2Templates(directory="src/templates")
 
 @router.get("/", response_class=HTMLResponse)
-async def index(request: Request):
+async def index(request: Request, response: Response):
+    response.set_cookie(key="language_code", value="ukr", httponly=True, samesite="None", secure=True)
     return templates.TemplateResponse("index.html", {"request": request, "title": "Головна"})
 
 
 @router.get("/index", response_class=HTMLResponse)
-async def index(request: Request):
+async def index(request: Request, response: Response):
+    response.set_cookie(key="language_code", value="ukr", httponly=True, samesite="None", secure=True)
     return templates.TemplateResponse("index.html", {"request": request, "title": "Головна"})
