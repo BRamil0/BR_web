34e9262d1ba969cc2795b769cc7b590bafc6f90c
Radomyr <qulowg@gmail.com>
2024-10-15 15:18:01 +0300

    - final

--- src/app/fastapi/base.py
+++ src/app/fastapi/base.py
@@ -12,31 +12,37 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "title": "Головна"})
 
 @router.get("/index")
-async def not_found():
+async def index2():
     return RedirectResponse(url="/")
 
 @router.get("/terms_of_use", response_class=HTMLResponse)
-async def index(request: Request):
+async def terms_of_use(request: Request):
     return templates.TemplateResponse("terms_of_use.html", {"request": request, "title": "Умови використання"})
 
 @router.get("/privacy_policy", response_class=HTMLResponse)
-async def index(request: Request):
+async def privacy_policy(request: Request):
     return templates.TemplateResponse("privacy_policy.html", {"request": request, "title": "Політика конфіденційності"})
 
 @router.get("/cookie_policy", response_class=HTMLResponse)
-async def index(request: Request):
+async def cookie_policy(request: Request):
     return templates.TemplateResponse("cookie_policy.html", {"request": request, "title": "Політика використання cookie"})
 
 @router.get("/content_used", response_class=HTMLResponse)
-async def index(request: Request):
+async def content_used(request: Request):
     return templates.TemplateResponse("content_used.html", {"request": request, "title": "Використаний контент"})
 
-
 @router.get("/contact", response_class=HTMLResponse)
-async def index(request: Request):
+async def contact(request: Request):
     return templates.TemplateResponse("contact.html", {"request": request, "title": "Контакти"})
 
+@router.get("/project", response_class=HTMLResponse)
+async def project(request: Request):
+    return templates.TemplateResponse("project.html", {"request": request, "title": "Мої проєкти"})
+
+@router.get("/about", response_class=HTMLResponse)
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request, "title": "Про мене"})
 
 @router.get("/418", status_code=status.HTTP_418_IM_A_TEAPOT)
-async def index(request: Request):
+async def code_418(request: Request):
     return templates.TemplateResponse("code.html", {"request": request, "title": "code 418", "code": 418, "message": "I'm a teapot"})
