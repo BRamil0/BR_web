3673e98d7d74c1dd567600daa5ce0e8e07d9658e
Radomyr <qulowg@gmail.com>
2024-10-16 19:30:11 +0300

    - fix

--- src/app/fastapi/background.py
+++ src/app/fastapi/background.py
@@ -15,6 +15,6 @@ async def background():
         if int(ran) not in not_use:
             break
 
-    return {"image4k": f"/static/image/background/4k/{ran}.webp",
+    return {"image4k": f"/static/image/background/jpg/{ran}.jpg",
             "image2k": f"/static/image/background/2k/{ran}.webp",
             "image1k": f"/static/image/background/1k/{ran}.webp",}
