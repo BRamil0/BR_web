9726da8cb9ef425a5d8ea82697cf6069daccf9c8
Radomyr <qulowg@gmail.com>
2024-10-26 22:25:37 +0300

    - add Open Graph

--- src/app/fastapi/base.py
+++ src/app/fastapi/base.py
@@ -11,7 +11,9 @@ templates = Jinja2Templates(directory="src/templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "title": "Головна"})
+    return templates.TemplateResponse("index.html", {"request": request,
+                                                     "title": "Головна",
+                                                     "link": "index"})
 
 @router.get("/index")
 async def home():
@@ -19,32 +21,50 @@ async def home():
 
 @router.get("/terms_of_use", response_class=HTMLResponse)
 async def terms_of_use(request: Request):
-    return templates.TemplateResponse("terms_of_use.html", {"request": request, "title": "Умови використання"})
+    return templates.TemplateResponse("terms_of_use.html", {"request": request,
+                                                            "title": "Умови використання",
+                                                            "link": "terms_of_use"})
 
 @router.get("/privacy_policy", response_class=HTMLResponse)
 async def privacy_policy(request: Request):
-    return templates.TemplateResponse("privacy_policy.html", {"request": request, "title": "Політика конфіденційності"})
+    return templates.TemplateResponse("privacy_policy.html", {"request": request,
+                                                              "title": "Політика конфіденційності",
+                                                              "link": "privacy_policy"})
 
 @router.get("/cookie_policy", response_class=HTMLResponse)
 async def cookie_policy(request: Request):
-    return templates.TemplateResponse("cookie_policy.html", {"request": request, "title": "Політика використання cookie"})
+    return templates.TemplateResponse("cookie_policy.html", {"request": request,
+                                                             "title": "Політика використання cookie",
+                                                             "link": "cookie_policy"})
 
 @router.get("/content_used", response_class=HTMLResponse)
 async def content_used(request: Request):
-    return templates.TemplateResponse("content_used.html", {"request": request, "title": "Використаний контент"})
+    return templates.TemplateResponse("content_used.html", {"request": request,
+                                                            "title": "Використаний контент",
+                                                            "link": "content_used"})
 
 @router.get("/contact", response_class=HTMLResponse)
 async def contact(request: Request):
-    return templates.TemplateResponse("contact.html", {"request": request, "title": "Контакти"})
+    return templates.TemplateResponse("contact.html", {"request": request,
+                                                       "title": "Контакти",
+                                                       "link": "contact"})
 
 @router.get("/project", response_class=HTMLResponse)
 async def project(request: Request):
-    return templates.TemplateResponse("project.html", {"request": request, "title": "Мої проєкти"})
+    return templates.TemplateResponse("project.html", {"request": request,
+                                                       "title": "Мої проєкти",
+                                                       "link": "project"})
 
 @router.get("/about", response_class=HTMLResponse)
 async def about(request: Request):
-    return templates.TemplateResponse("about.html", {"request": request, "title": "Про мене"})
+    return templates.TemplateResponse("about.html", {"request": request,
+                                                     "title": "Про мене",
+                                                     "link": "about"})
 
 @router.get("/418", status_code=status.HTTP_418_IM_A_TEAPOT)
 async def code_418(request: Request):
-    return templates.TemplateResponse("code.html", {"request": request, "title": "code 418", "code": 418, "message": "I'm a teapot"})
+    return templates.TemplateResponse("code.html", {"request": request,
+                                                    "title": "code 418",
+                                                    "code": 418,
+                                                    "message": "I'm a teapot",
+                                                    "link": "418"})
