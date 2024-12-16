f7fdac68cde35fddf34bb3051fd89735842a855e
Radomyr <qulowg@gmail.com>
2024-12-16 16:36:12 +0200

    a page for creating a blog has been added

--- src/fastapi_app/blog.py
+++ src/fastapi_app/blog.py
@@ -4,6 +4,7 @@ import typing
 from fastapi import APIRouter, Depends, Request
 from starlette.responses import HTMLResponse, RedirectResponse
 
+import auth_utils
 from src.database.database import DataBase, SearchTypeForPost
 from src.backend.templates import templates
 
@@ -36,7 +37,13 @@ async def post_redirect():
     return RedirectResponse(url="/post_list")
 
 
-@router.get("/post/{post_id}", response_class=HTMLResponse)
-async def get_post(request: Request, post_id: int, db: DataBase = Depends(get_database)):
-    post = await db.get_post(SearchTypeForPost.id, post_id)
+@router.get("/post/{post_url}", response_class=HTMLResponse)
+async def get_post(request: Request, post_url: str, db: DataBase = Depends(get_database)):
+    post = await db.get_post(SearchTypeForPost.url, post_url)
+    if not post: post = await db.get_post(SearchTypeForPost.id, post_url)
     return templates.TemplateResponse("post.html", {"request": request, "post": post[0]})
+
+@router.get("/create_post", response_class=HTMLResponse)
+async def post_create(request: Request, token_data: dict = Depends(auth_utils.token_verification_no_exceptions)):
+    if not token_data: return RedirectResponse("/account/login", status_code=302)
+    return templates.TemplateResponse("post_create.html", {"request": request})
