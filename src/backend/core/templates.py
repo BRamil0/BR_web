5620f889a836106be2df98039a3060a7167e1844
Radomyr <qulowg@gmail.com>
2025-01-17 15:03:56 +0200

    reddened about_site.html

--- src/backend/core/templates.py
+++ src/backend/core/templates.py
@@ -5,3 +5,4 @@ templates = Jinja2Templates(directory="src/frontend/templates")
 templates.env.globals["DEBUG_MODE"] = settings.DEBUG
 templates.env.globals["experimental_functions"] = settings.experimental_functions
 templates.env.globals["server_version"] = settings.server_version
+templates.env.globals["api_version"] = settings.api_version
