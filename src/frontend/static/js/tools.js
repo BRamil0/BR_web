acde3424e1eb5ec566c4656d397a6f1e5914bbae
Radomyr <qulowg@gmail.com>
2025-01-29 23:46:57 +0200

    ще один баг фікс

--- src/frontend/static/js/tools.js
+++ src/frontend/static/js/tools.js
@@ -1,4 +1,4 @@
 export async function getURL(API_URL) {
     const protocol = window.location.protocol === 'https:' ? 'https:' : 'http:';
-    return `${protocol}//${window.location.host}/${API_URL}`;
+    return `${protocol}//${window.location.host}${API_URL}`;
 }
