7e195da86aa236f6095db0b45dfb12fc7b8bea48
Radomyr <qulowg@gmail.com>
2025-01-30 01:04:15 +0200

    виправлення проблеми з ssl та https

--- src/frontend/static/js/tools.js
+++ src/frontend/static/js/tools.js
@@ -1,4 +1,4 @@
 export async function getURL(API_URL) {
-    const protocol = window.location.protocol === 'https:' ? 'https:' : 'http:';
+    const protocol = 'https:';
     return `${protocol}//${window.location.host}${API_URL}`;
 }
