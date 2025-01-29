e6ad474c217c90aa9aca3a08e68e83864739514e
Radomyr <qulowg@gmail.com>
2025-01-30 01:49:09 +0200

    повернул старий код

--- src/frontend/static/js/tools.js
+++ src/frontend/static/js/tools.js
@@ -1,4 +1,4 @@
 export async function getURL(API_URL) {
-    const protocol = 'https:';
+    const protocol = window.location.protocol === 'https:' ? 'https:' : 'http:';
     return `${protocol}//${window.location.host}${API_URL}`;
 }
